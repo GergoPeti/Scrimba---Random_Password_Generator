4d9e3de56acd5eae15f011ac5a61aa8fa3cd168d
Gergő Peti <petigergo97@gmail.com>
2023-03-21 20:20:18 +0100

    Random Password Generator
    Scrimba Frontend Developer Path -Module 3 Solo Project

---
+++ script.js
@@ -0,0 +1,123 @@
+'use strict';
+
+const characters = [
+  'A',
+  'B',
+  'C',
+  'D',
+  'E',
+  'F',
+  'G',
+  'H',
+  'I',
+  'J',
+  'K',
+  'L',
+  'M',
+  'N',
+  'O',
+  'P',
+  'Q',
+  'R',
+  'S',
+  'T',
+  'U',
+  'V',
+  'W',
+  'X',
+  'Y',
+  'Z',
+  'a',
+  'b',
+  'c',
+  'd',
+  'e',
+  'f',
+  'g',
+  'h',
+  'i',
+  'j',
+  'k',
+  'l',
+  'm',
+  'n',
+  'o',
+  'p',
+  'q',
+  'r',
+  's',
+  't',
+  'u',
+  'v',
+  'w',
+  'x',
+  'y',
+  'z',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+  '~',
+  '`',
+  '!',
+  '@',
+  '#',
+  '$',
+  '%',
+  '^',
+  '&',
+  '*',
+  '(',
+  ')',
+  '_',
+  '-',
+  '+',
+  '=',
+  '{',
+  '[',
+  '}',
+  ']',
+  ',',
+  '|',
+  ':',
+  ';',
+  '<',
+  '>',
+  '.',
+  '?',
+  '/',
+];
+
+const btnEl = document.querySelector('.btn');
+const firstPasswordEl = document.querySelector('.first-pw');
+const secondPasswordEl = document.querySelector('.second-pw');
+
+const inputFieldEl = document.querySelector('.input-pw_container');
+
+const randomIndex = function () {
+  return Math.floor(Math.random() * characters.length);
+};
+
+btnEl.addEventListener('click', function () {
+  let pwLengthSetByUser;
+
+  if (Number(inputFieldEl.value) === 0) {
+    pwLengthSetByUser = 15;
+  } else {
+    pwLengthSetByUser = Number(inputFieldEl.value);
+  }
+
+  firstPasswordEl.textContent = '';
+  secondPasswordEl.textContent = '';
+
+  for (let i = 0; i < pwLengthSetByUser; i++) {
+    firstPasswordEl.textContent += characters[randomIndex()];
+    secondPasswordEl.textContent += characters[randomIndex()];
+  }
+});
